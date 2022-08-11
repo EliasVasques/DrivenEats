c9436f02cec20291139b74267344356e1ea8cb16
EliasVasques <eliasvasques74@gmail.com>
2022-08-11 16:22:15 -0300

    item selecionado funcionando em todas sessões, e botão de fechar pedido ficando mudando ao selecionar item em todas sessões

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function itemEscolhido(itemSelecionado) {
+
+
+function itemEscolhido(itemSelecionado, tipoItem) {
     let cont = 0;
     while (true) {
-        const elemento = document.getElementsByClassName('prato')[cont];
+        const elemento = document.getElementsByClassName(tipoItem)[cont];
 
         if (elemento === undefined) break
 
@@ -15,4 +17,44 @@ function itemEscolhido(itemSelecionado) {
     else {
         itemSelecionado.classList.add('item-selecionado');
     }
+
+    if(podeFecharPedido()) {
+        const fecharPedido = document.querySelector('.fechar-pedido');
+        fecharPedido.innerHTML = 'Fechar Pedido'
+        fecharPedido.style.backgroundColor = '#32B72F';
+    }
+
+
+}
+
+const podeFecharPedido = () => {
+
+    let temPrato = false;
+    const pratos = document.getElementsByClassName('prato');
+    for (let i=0; i < pratos.length; i++) {
+        if(pratos[i].classList.contains('item-selecionado')) {
+            temPrato = true;
+            break;
+        }
+    }
+
+    let temBebida = false;
+    const bebidas = document.getElementsByClassName('bebida');
+    for (let i=0; i < bebidas.length; i++) {
+        if(bebidas[i].classList.contains('item-selecionado')) {
+            temBebida = true;
+            break;
+        }
+    }
+    let temSobremesa = false;
+    const sobremesas = document.getElementsByClassName('sobremesa');
+    for (let i=0; i < sobremesas.length; i++) {
+        if(sobremesas[i].classList.contains('item-selecionado')) {
+            temSobremesa = true;
+            break;
+        }
+    }
+
+    if(temPrato && temBebida && temSobremesa) return true;
+    else return false;
 }
